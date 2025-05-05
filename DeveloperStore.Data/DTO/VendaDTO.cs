63be77a8eb5f935713e453dbb223cf4ae45ad2e3
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-05-05 12:23:57 -0300

    Desenvolvendo o DTO e o automapper fazendo sua configuração no projeto

---
+++ DeveloperStore.Data/DTO/VendaDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperStore.Data.DTO
+{
+    public class VendaDTO
+    {
+        public int Id { get; set; }
+
+        public int NumeroVenda { get; set; }
+
+        public DateTime DataTotalVenda { get; set; }
+
+        public string? Cliente { get; set; }
+
+        public double ValorTotalVenda { get; set; }
+
+        public string? FilialVenda { get; set; }
+
+        public string? Produto { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public decimal PrecoUnitario { get; set; }
+
+        public double Desconto { get; set; }
+
+        public double ValorTotalItem { get; set; }
+    }
+}
